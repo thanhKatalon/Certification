6c22073296aace05629e64ad966e2b9b98445ba4
thanh.pnguyen <thanh.pnguyen@10.10.32.101>
2025-02-11 10:41:41 +0700

    test suite update

--- Test Suites/TS-01.ts
+++ Test Suites/TS-01.ts
@@ -12,10 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c21f6ddd-a07b-4ae4-b3b5-3a6aec77d007</testSuiteGuid>
    <testCaseLink>
-      <guid>bd5d5447-3ed0-494e-b317-04bf98e9ac43</guid>
+      <guid>86784469-eccc-41a3-8878-ab8481c222f1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Account/open browser</testCaseId>
+      <testCaseId>Test Cases/TC_upload file</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
