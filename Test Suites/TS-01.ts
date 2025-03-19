084941324229be63e73a98ac7954d584fa8a6e8f
thanh.pnguyen <thanh.pnguyen@125.235.4.59.adsl.viettel.vn>
2025-03-19 23:01:48 +0700

    tes suite

--- Test Suites/TS-01.ts
+++ Test Suites/TS-01.ts
@@ -5,17 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>1</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>c21f6ddd-a07b-4ae4-b3b5-3a6aec77d007</testSuiteGuid>
    <testCaseLink>
-      <guid>b222dc8e-5ed1-4f5d-bc8a-447020ac1c02</guid>
+      <guid>fdedb0b7-a145-422a-8eeb-bfe6fde25e1b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Add to Cart - Samsung</testCaseId>
+      <testCaseId>Test Cases/Jazz Cash_Sroll to Text</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b7d292fe-dbd0-46e5-8061-9cf42d76e714</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jazz Cash_Sroll to Text - Copy</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
